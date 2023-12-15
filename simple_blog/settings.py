f4f93c50f2b8dcaae9a55af53bdc435564c518c7
Javier Hernandez <=>
2023-12-16 00:01:10 +0100

    static files

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -125,13 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
